0e0d2fc9dc1f0c4e77889e56f9ef1bca48aa91bb
ardeshir sepahsalar <ardeshir.org@gmail.com>
2017-07-30 20:37:28 +0000

    using range

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 import (
      "fmt"
-     "time"
+    // "time"
 )
 
 var unbuff = make(chan int)
-var   buff = make(chan int)
+var   buff = make(chan int, 5)
 
 func main() {
     count :=5
     go func() {
 	for i := 0; i <= count; i++ {
-		fmt.Println("Sending message: ")
-		unbuff <- i
+		fmt.Println("Sending message: ", i)
+		buff <- i
 	}
+       close(buff)
     }()
 
-    time.Sleep(time.Second * 3)
-    for i := 0; i <= count; i++ {
-      fmt.Println("Receiving: ")
-		fmt.Println( <- unbuff)
+    // time.Sleep(time.Second * 3)
+    for num := range buff {
+      fmt.Print("Receiving: ")
+		fmt.Println(num)
     }
 }
